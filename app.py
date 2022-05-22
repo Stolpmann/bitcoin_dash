12f18b7031f5880700602d9bb8d3d45c36ea8d8a
Stolpmann <evan.stolpmann@hotmail.com>
2022-05-22 15:26:37 -0400

    test with sample data

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
 
 from dash import Dash, dcc, html
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 app = Dash(__name__)
 
 colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+    'background': '#15202b',
+    'text': '#FF9900'
 }
+
+df = pd.read_csv ('clean_block_data.csv')
+print(df)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-fig.update_layout(
+fig1 = px.line(df, x="height", y="txs", title='Bitcoin Transactions')
+
+fig1.update_traces(line_color='#FF9900', line_width=3)
+
+fig2 = px.line(df, x="height", y="txs", title='Bitcoin Transactions')
+
+fig2.update_traces(line_color='#FF9900', line_width=3)
+
+fig1.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+fig2.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
     font_color=colors['text']
@@ -28,21 +41,38 @@ fig.update_layout(
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(
-        children='Hello Dash',
+        children='Bitcoin Dashboard',
         style={
             'textAlign': 'center',
             'color': colors['text']
         }
     ),
 
-    html.Div(children='Dash: A web application framework for your data.', style={
+    html.Div(children='', style={
         'textAlign': 'center',
         'color': colors['text']
     }),
 
     dcc.Graph(
         id='example-graph-2',
-        figure=fig
+        figure=fig1
+    ),
+    html.H1(
+        children='Bitcoin Dashboard',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='example-graph-3',
+        figure=fig2
     )
 ])
 
