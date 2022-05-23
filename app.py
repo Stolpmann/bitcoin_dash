cba9b00f18f1b2c32c31ee00dc7fceacb44cd33c
Stolpmann <evan.stolpmann@hotmail.com>
2022-05-23 17:33:01 -0400

    styled charts

--- app.py
+++ app.py
@@ -15,11 +15,14 @@ colors = {
 
 df = pd.read_csv ('clean_block_data.csv')
 supply = pd.read_csv('supply.csv')
+halving = pd.read_csv('halvings.csv')
 print(df)
 
 bitcoin_supply =[supply['issuance_remaining'][0], int(supply['total_amount'][0])]
 
-bitcoin_supply_titles = ['issuance_remaining', 'Circulating_supply']
+bitcoin_supply_titles = ['Issuance Remaining', 'Circulating Supply']
+
+colors2 = ['#4D4D4D', '#FF9900']
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
@@ -27,40 +30,46 @@ bitcoin_supply_titles = ['issuance_remaining', 'Circulating_supply']
 
 fig1 = px.line(df, x="height", y="txs", title='Bitcoin Transactions')
 
-fig1.update_traces(line_color='#FF9900', line_width=3)
+fig1.update_traces(line_color='#FF9900', line_width=3, )
 
-fig2 = px.line(df, x="height", y="txs", title='Bitcoin Transactions')
+fig2 = px.line(halving, x="Year", y="Start BTC", title='Projected Halvings',log_x=True)
 
 fig2.update_traces(line_color='#FF9900', line_width=3)
 
 fig3 = go.Figure(data=[go.Pie(labels=bitcoin_supply_titles, values=bitcoin_supply)])
 
+fig3.update_traces(marker=dict(colors=colors2,line=dict(color='#FFFFFF', width=1)))
 
 
 
 fig1.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
-    font_color=colors['text']
+    font_color=colors['text'],
+    title=dict(font_size=20)
 )
 fig2.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
-    font_color=colors['text']
+    font_color=colors['text'],
+    title = dict(font_size=20)
+
 )
 
 fig3.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
-    font_color=colors['text']
+    font_color=colors['text'],
+    title=dict(text='Bitcoin Supply', font_size=20)
 )
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+app.layout = html.Div(style={'backgroundColor': colors['background'], 'marginLeft': -10, 'marginRight': -10, 'marginTop': -25}, children=[
     html.H1(
         children='Bitcoin Dashboard',
         style={
             'textAlign': 'center',
-            'color': colors['text']
+            'color': colors['text'],
+            'paddingTop': 15
         }
     ),
 
@@ -74,10 +83,11 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         figure=fig1
     ),
     html.H1(
-        children='Bitcoin Dashboard',
+        children='Supply',
         style={
             'textAlign': 'center',
-            'color': colors['text']
+            'color': colors['text'],
+            'paddingTop': 15
         }
     ),
 
@@ -91,14 +101,18 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         figure=fig2
     ),
 
-    html.Div(children='', style={
-        'textAlign': 'center',
-        'color': colors['text']
+
+    html.Div(
+        children='',
+        style={
+            'textAlign': 'center',
+            'color': colors['text'],
+            'paddingTop': -30
     }),
 
     dcc.Graph(
         id='example-graph-4',
-        figure=fig3
+        figure=fig3,
     )
 ])
 
